c4d543e78de869a689d41db9569fe1b1be6f3208
NavyaSriSura <navyasura98@gmail.com>
2019-07-29 12:40:49 +0530

    mongoDB first

--- src/main/java/com/stackroute/SpringBootTask/domain/Music.java
+++ src/main/java/com/stackroute/SpringBootTask/domain/Music.java
@@ -1,49 +1,27 @@
 package com.stackroute.SpringBootTask.domain;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Music {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+   // @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
     private int rating;
     private String comments;
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
-
-    public String getComments() {
-        return comments;
-    }
-
-    public void setComments(String comments) {
-        this.comments = comments;
-    }
 }
--- src/main/java/com/stackroute/SpringBootTask/repository/MusicRepository.java
+++ src/main/java/com/stackroute/SpringBootTask/repository/MusicRepository.java
@@ -3,10 +3,17 @@ package com.stackroute.SpringBootTask.repository;
 import com.stackroute.SpringBootTask.domain.Music;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Collection;
 import java.util.List;
 
-public interface MusicRepository extends JpaRepository<Music, Integer> {
+public interface MusicRepository extends MongoRepository<Music, Integer> {
+    /*@Query(
+            value = "SELECT * FROM MUSIC",
+            nativeQuery = true)
+    Collection<Music> findAllActiveUsers();*/
 
+    @Query("{name:'?0'")
+    List<Music> findTitleByName(String name);
 }
